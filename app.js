93562c8c0f83d3eb54fc53e2d88fe4edbdf8e96e
CarsonRey <c.rey494@gmail.com>
2019-04-30 17:28:07 -0400

    introducing file system module to write html

--- app.js
+++ app.js
@@ -2,20 +2,34 @@
 
 // http is a core module in node.js so we can just call it here without having to do 'npm install'
 const http = require('http');
+// 'file system' is also a core module
+const fs = require('fs');
 
 const hostname = '192.168.86.29';
 const port = 3000;
 
-// we're calling .createServer on the module (which I'm guessing is a native function included in node.js --  included in the http module according to the video)
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World')
+
+// stands for "file system"
+fs.readFile('index.html', (err, html) => {
+  if(err){
+    throw err;
+  }
+  // we're calling .createServer on the module (which I'm guessing is a native function included in node.js --  included in the http module according to the video)
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    // we change from text/plain to text/html to read the html that we're writing on line 19
+    res.setHeader('Content-Type', 'text/html')
+    res.write(html)
+    res.end()
+  })
+
+  server.listen(port, hostname, () => {
+    console.log('Server started on port ' + port)
+  })
+
 })
 
-server.listen(port, hostname, () => {
-  console.log('Server started on port ' + port)
-})
+
 
 // From here we can:
 // run 'node app' to run the file
