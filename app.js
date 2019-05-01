c7db8a78d01ef0523334fb4d559fd3eed6cf9a23
CarsonRey <c.rey494@gmail.com>
2019-05-01 13:04:43 -0400

    working with events module

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
 // This is how to make a basic server with node.js
 
 // http is a core module in node.js so we can just call it here without having to do 'npm install'
-const http = require('http');
-// 'file system' is also a core module
-const fs = require('fs');
-
-const hostname = '192.168.86.29';
-const port = 3000;
-
-
-// stands for "file system"
-fs.readFile('index.html', (err, html) => {
-  if(err){
-    throw err;
-  }
-  // we're calling .createServer on the module (which I'm guessing is a native function included in node.js --  included in the http module according to the video)
-  const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    // we change from text/plain to text/html to read the html that we're writing on line 19
-    res.setHeader('Content-Type', 'text/html')
-    res.write(html)
-    res.end()
-  })
-
-  server.listen(port, hostname, () => {
-    console.log('Server started on port ' + port)
-  })
-
-})
-
-
+// const http = require('http');
+// // 'file system' is also a core module
+// const fs = require('fs');
+//
+// const hostname = '192.168.86.29';
+// const port = 3000;
+//
+//
+// // stands for "file system"
+// fs.readFile('index.html', (err, html) => {
+//   if(err){
+//     throw err;
+//   }
+//   // we're calling .createServer on the module (which I'm guessing is a native function included in node.js --  included in the http module according to the video)
+//   const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     // we change from text/plain to text/html to read the html that we're writing on line 19
+//     res.setHeader('Content-Type', 'text/html')
+//     res.write(html)
+//     res.end()
+//   })
+//
+//   server.listen(port, hostname, () => {
+//     console.log('Server started on port ' + port)
+//   })
+//
+// })
 
 // From here we can:
-// run 'node app' to run the file
+// run 'node [filename]' to run the file
 // Access the response by going to localhost:3000 or hostname:3000
+
+// --------------------------------------
+// WORKING WITH EVENTS MODULE
+// --------------------------------------
+
+// we grab the event EventEmitter class from the events module
+const EventEmitter = require('events')
+// create a new instance
+const emitter = new EventEmitter
+
+// Adding an event listener
+emitter.on(`Greeting`, (name) => {
+  console.log(`Hey there ${name}`)
+})
+
+// Adding an event with .emit
+emitter.emit('Greeting', "Carson")
